192b19a482e19a5ccf94dd7b596f5b68101121fc
sjmr0911 <sjmr8586@gmail.com>
2025-02-14 14:48:29 -0400

    Documentacion codigo Pruebas Banco autenticación MFA & Codigo dinamico

--- banco_Autentication_MFA/tests/Autenticacion_Tests.cs
+++ banco_Autentication_MFA/tests/Autenticacion_Tests.cs
@@ -19,3 +19,16 @@ public class AuthenticationTests
         Assert.False(result);
     }
 }
+
+# Documentación
+
+## Propósito de esta prueba unitaria
+
+### **Banco (Autenticación MFA y Generación de codigo dinamico)**
+- **Authenticacion_Tests.cs**: Estas pruebas validan la autenticación multifactor del usuario, asegurando que solo aquellos con credenciales válidas puedan acceder a su cuenta.
+  - **Caso de Uso 1: Inicio de sesión exitoso con MFA**: Un usuario con credenciales correctas y un código MFA válido debe poder iniciar sesión exitosamente.
+  - **Caso de Uso 2: Fallo en la autenticación por credenciales incorrectas**: Si un usuario proporciona credenciales incorrectas o un código MFA inválido, el acceso debe ser denegado.
+
+- **AuthService.cs**: Valida la correcta generación de códigos de autenticación.
+  - **Caso de Uso 3: Generación de códigos dinámicos**: El sistema debe generar códigos únicos para cada intento de autenticación.
+  - **Caso de Uso 4: Expiración de códigos MFA**: Un código generado debe expirar después de cierto tiempo, evitando su reutilización.
